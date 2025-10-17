2abebb58b0453bc941bd45b539791297fa1d074e
Jefferson de Souza Batista <jeffsouzabatista@gmail.com>
2025-10-17 20:22:48 -0300

    Initial commit – NindoFast base setup (SwiftData + Swift Testing)

---
+++ NindoFast/NindoFastApp.swift
@@ -0,0 +1,32 @@
+//
+//  NindoFastApp.swift
+//  NindoFast
+//
+//  Created by Jefferson de Souza Batista on 16/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NindoFastApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ NindoFastTests/NindoFastTests.swift
@@ -0,0 +1,16 @@
+//
+//  NindoFastTests.swift
+//  NindoFastTests
+//
+//  Created by Jefferson de Souza Batista on 16/10/25.
+//
+
+import Testing
+
+struct NindoFastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
